8f440b408278d47ff9b3c025b18593fff31667db
marksangil-ekonek <mark.sangil@ekonek.com>
2025-03-18 16:10:28 +0800

    Added weather feature (60%)

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'Provider + Supabase',
+      title: 'StatTrak',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
